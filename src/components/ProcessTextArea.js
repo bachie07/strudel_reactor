abfbb136c30d7893ad52feee5c9df93d20a550f3
bachie07 <giabach0704@gmail.com>
2025-11-04 22:50:35 +1030

    Create ProcessTextArea.js
    Create ProcessTextArea component to refactor currnet app.js code

---
+++ src/components/ProcessTextArea.js
@@ -0,0 +1,12 @@
+function ProcessTextArea(){
+
+    return(
+        <div>
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
+        <textarea className="form-control" rows="15" id="proc" ></textarea>
+        </div>
+    );
+}
+
+export default ProcessTextArea;
+
