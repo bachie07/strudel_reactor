25652b8fc518166e7e327c72b7dba61ad90e2745
bachie07 <giabach0704@gmail.com>
2025-11-05 21:26:16 +1030

    Update preprocessor controls
    use p1Enabled and onP1Change to manage state of preprocessor

--- src/components/PreprocessorControls.js
+++ src/components/PreprocessorControls.js
@@ -1,24 +1,28 @@
-function PreprocessorControls(){
+function PreprocessorControls({ p1Enabled, onP1Change }){
  // onChange={ProcAndPlay} defaultChecked  ->(for ON) input
  // onChange={ProcAndPlay} (for HUSH) -> input
     return (
 
         <div className="col-md-4">
         <div className="form-check">
-            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"  /> 
+            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked={p1Enabled === true} onChange={() => {   console.log("Radio clicked -> p1Enabled:", true);
+onP1Change(true)} }/> 
             <label className="form-check-label" htmlFor="flexRadioDefault1">
                 p1: ON
             </label>
         </div>
         <div className="form-check">
-            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"  />
+            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked={p1Enabled === false} onChange={() => onP1Change(false)} />
             <label className="form-check-label" htmlFor="flexRadioDefault2">
                 p1: HUSH
             </label>
         </div>
-    </div>
+        <br></br>
 
-    )
+
+            </div>
+
+    );
 }
 
 export default PreprocessorControls;
