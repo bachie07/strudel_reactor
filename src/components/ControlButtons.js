94d4e4bcd2c94cce868ad45bedcd22ebd13457b4
bachie07 <giabach0704@gmail.com>
2025-11-05 21:24:10 +1030

    Update controlbutton.js
    Change the layout, also pass props into button onclicks

--- src/components/ControlButtons.js
+++ src/components/ControlButtons.js
@@ -1,7 +1,27 @@
+function ControlButtons( { onPlay, onStop, onProcess, onProcessAndPlay }){
+
+    console.log("ControlButtons rendered");
 
     return (
         <nav>
+            <div className="row"> 
+                <div className="col-auto">
+                    <button id="play" className="btn btn-outline-primary" onClick={onPlay}>Play</button>
+                </div>
+                <div className="col-auto">
+                <button id="stop" className="btn btn-outline-danger" onClick={onStop}>Stop</button>
+                </div>
+                <div className="col-auto">
+                    <button id="process" className="btn btn-outline-primary" onClick={onProcess}>Preprocess</button>
+                </div>
+                <div className="col-auto">
+                    <button id="process_play" className="btn btn-outline-primary" onClick={onProcessAndPlay}>Process & Play</button>
+                </div>
+
+            </div>
+
         </nav>
+        
 
     );
 }
