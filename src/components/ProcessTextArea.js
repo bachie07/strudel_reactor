62e88e580b3c47b72ebb30e6526f1a54de33d1d0
bachie07 <giabach0704@gmail.com>
2025-11-05 21:22:38 +1030

    Pass props to ProcessTextArea
    Set default value for text area and manage change

--- src/components/ProcessTextArea.js
+++ src/components/ProcessTextArea.js
@@ -1,9 +1,9 @@
-function ProcessTextArea(){
+function ProcessTextArea( { textValue, onTextChange }){
 
     return(
         <div>
         <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
-        <textarea className="form-control" rows="15" id="proc" ></textarea>
+        <textarea className="form-control" rows="15" defaultValue={textValue} onChange={onTextChange} id="proc" ></textarea>
         </div>
     );
 }
