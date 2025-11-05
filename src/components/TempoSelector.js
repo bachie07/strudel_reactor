d1bc1b2bbfbffccf5e6fd9168b4c139a826188c0
bachie07 <giabach0704@gmail.com>
2025-11-05 21:25:04 +1030

    Create tempo selector.js
    Create tempo selector , move from app.js

---
+++ src/components/TempoSelector.js
@@ -0,0 +1,12 @@
+function TempoSelector(){
+
+    return(
+
+        <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">setCPM</span>
+        <input type="text" className="form-control" placeholder="120" aria-label="Username" aria-describedby="cpm_label" style={{maxWidth: 100}}/>
+        </div> 
+    )
+}
+
+export default TempoSelector;
