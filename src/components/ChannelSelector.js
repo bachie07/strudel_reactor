1c3ee8e86993a41906f7e94ae92107baf813fb32
bachie07 <giabach0704@gmail.com>
2025-11-05 21:24:45 +1030

    Create ChannelSelector.js
    Create new component for channel selector feature (move from app.js)

---
+++ src/components/ChannelSelector.js
@@ -0,0 +1,32 @@
+function ChannelSelector(){
+
+    return(
+
+        <div>
+        <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="s1"/>
+        <label className="form-check-label" htmlFor="s1">
+        s1
+        </label>
+        </div>
+
+        <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="d1"/>
+        <label className="form-check-label" htmlFor="d1">
+            d1
+        </label>
+        </div>
+
+        <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="d2"/>
+        <label className="form-check-label" htmlFor="d2">
+            d2
+        </label>
+        </div> 
+
+         </div>
+
+    )
+}
+
+export default ChannelSelector;
