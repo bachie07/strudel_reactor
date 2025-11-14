1c403a4deda3835260389db9f7ab276bf451e434
bachie07 <giabach0704@gmail.com>
2025-11-14 23:43:25 +1030

    Update Preprocessor
    Use switch instead

--- src/components/PreprocessorControls.js
+++ src/components/PreprocessorControls.js
@@ -3,22 +3,24 @@ function PreprocessorControls({ p1Enabled, onP1Change }){
  // onChange={ProcAndPlay} (for HUSH) -> input
     return (
 
-        <div className="col-md-4">
-        <div className="form-check">
-            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked={p1Enabled === true} onChange={() => {   console.log("Radio clicked -> p1Enabled:", true);
-onP1Change(true)} }/> 
-            <label className="form-check-label" htmlFor="flexRadioDefault1">
-                p1: ON
-            </label>
-        </div>
-        <div className="form-check">
-            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked={p1Enabled === false} onChange={() => onP1Change(false)} />
-            <label className="form-check-label" htmlFor="flexRadioDefault2">
-                p1: HUSH
-            </label>
-        </div>
-        <br></br>
-
+        <div>
+            <br></br>
+            <div className="form-check form-switch">
+            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDefault" checked/>
+            <label className="form-check-label" htmlFor="switchCheckDefault">Main_arp</label>
+            </div>
+            <div class="form-check form-switch">
+            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckChecked" checked/>
+            <label className="form-check-label"  htmlFor="switchCheckChecked">Bass_line</label>
+            </div>
+            <div class="form-check form-switch">
+            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDisabled" checked/>
+            <label className="form-check-label"  htmlFor="switchCheckDisabled">Drums</label>
+            </div>
+            <div className="form-check form-switch">
+            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckCheckedDisabled" checked/>
+            <label className="form-check-label" htmlFor="switchCheckCheckedDisabled">Drums 2</label>
+            </div>
 
             </div>
 
