8e72682db9e47f76aa2e08107565f0b8983c1a95
bachie07 <giabach0704@gmail.com>
2025-11-16 18:16:55 +1030

    Update PreprocessorControl
    Pass channelsenabled and onchannelchange props to handle channel switches when user interaact

--- src/components/PreprocessorControls.js
+++ src/components/PreprocessorControls.js
@@ -1,24 +1,32 @@
-function PreprocessorControls({ p1Enabled, onP1Change }){
- // onChange={ProcAndPlay} defaultChecked  ->(for ON) input
- // onChange={ProcAndPlay} (for HUSH) -> input
+function PreprocessorControls({ channelsEnabled,  onChannelChange}){
+
+
     return (
 
         <div>
             <br></br>
             <div className="form-check form-switch">
-            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDefault" checked/>
+            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDefault" 
+            checked={channelsEnabled.main_arp} onChange={(e) => onChannelChange('main_arp', e.target.checked)}/>
             <label className="form-check-label" htmlFor="switchCheckDefault">Main_arp</label>
+
             </div>
-            <div class="form-check form-switch">
-            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckChecked" checked/>
+            <div className="form-check form-switch">
+            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckChecked" 
+            checked={channelsEnabled.bassline} onChange={(e) => onChannelChange('bassline', e.target.checked)}/>
+                      
             <label className="form-check-label"  htmlFor="switchCheckChecked">Bass_line</label>
             </div>
             <div class="form-check form-switch">
-            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDisabled" checked/>
+            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDisabled" 
+            checked={channelsEnabled.drums} onChange={(e) => onChannelChange('drums', e.target.checked)}/>
+
             <label className="form-check-label"  htmlFor="switchCheckDisabled">Drums</label>
             </div>
             <div className="form-check form-switch">
-            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckCheckedDisabled" checked/>
+            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckCheckedDisabled"
+            checked={channelsEnabled.drums2} onChange={(e) => onChannelChange('drums2', e.target.checked)}/>
+                   
             <label className="form-check-label" htmlFor="switchCheckCheckedDisabled">Drums 2</label>
             </div>
 
