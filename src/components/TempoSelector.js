809a39fc2c0cec9eb036d9926e31c27d8577168f
bachie07 <giabach0704@gmail.com>
2025-11-16 18:17:14 +1030

    Update TempoSelecter
    update volume range

--- src/components/TempoSelector.js
+++ src/components/TempoSelector.js
@@ -4,7 +4,7 @@ function TempoSelector( {tempo, updateTempo}){
 
         <div className="input-group mb-3">
         <span className="input-group-text" id="basic-addon1">setCPM</span>
-        <input type="range" className="form-range" min="0" max="200" step="10" id="volume_range" value={tempo} onChange={updateTempo}/>
+        <input type="range" className="form-range" min="60" max="200" step="10" id="volume_range" value={tempo} onChange={updateTempo}/>
 
         </div> 
     )
