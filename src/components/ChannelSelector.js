807c1b0a4f40e9103dc4fed325151aa91cf9aa5e
bachie07 <giabach0704@gmail.com>
2025-11-16 15:18:20 +1030

    Update channel selector boostrap

--- src/components/ChannelSelector.js
+++ src/components/ChannelSelector.js
@@ -3,26 +3,23 @@ function ChannelSelector(){
     return(
 
         <div>
-        <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="s1"/>
-        <label className="form-check-label" htmlFor="s1">
-        s1
-        </label>
-        </div>
-
-        <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="d1"/>
-        <label className="form-check-label" htmlFor="d1">
-            d1
-        </label>
-        </div>
-
-        <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="d2"/>
-        <label className="form-check-label" htmlFor="d2">
-            d2
-        </label>
-        </div> 
+            
+            <div class="form-check form-switch">
+                <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDefault"/>
+                <label class="form-check-label" for="switchCheckDefault">Main_arp</label>
+                </div>
+                <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" role="switch" id="switchCheckChecked" />
+                <label class="form-check-label" for="switchCheckChecked">Bass_line</label>
+                </div>
+                <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" role="switch" id="switchCheckDisabled" />
+                <label class="form-check-label" for="switchCheckDisabled">Drums</label>
+                </div>
+                <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" role="switch" id="switchCheckCheckedDisabled" />
+                <label class="form-check-label" for="switchCheckCheckedDisabled">Drums 2</label>
+                </div>
 
          </div>
 
