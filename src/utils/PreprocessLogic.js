04fdacd637e2ba10b5485d152fc327240f3ae542
bachie07 <giabach0704@gmail.com>
2025-11-16 18:19:32 +1030

    Add preprocess logic to handle channel
    add "_" to mute, remove to keep playing

--- src/utils/PreprocessLogic.js
+++ src/utils/PreprocessLogic.js
@@ -1,3 +1,26 @@
 
 export function preprocessSong( songText, {channelsEnabled, tempo, volume }){ //inspo by teacher (legend)
 
+
+    let processedText = songText; 
+
+
+    if(channelsEnabled){ 
+
+        Object.entries(channelsEnabled).forEach(([channel, isEnabled]) => {
+
+            if(!isEnabled){
+                 // Channel is OFF - add underscore to mute
+                 const regex = new RegExp(`^(${channel}):`, 'gm');
+                 processedText = processedText.replace(regex, `_${channel}:`);
+            }
+            else{
+
+                // Channel is ON - remove underscore if it exists
+                const regex = new RegExp(`^_(${channel}):`, 'gm');
+                processedText = processedText.replace(regex, `${channel}:`);
+            }
+
+        });
+    }
+
